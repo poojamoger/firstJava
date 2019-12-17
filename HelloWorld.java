8e676b19c848e0ba00b2fcd2c23702f21f3cc7dc
poojamoger <58925244+poojamoger@users.noreply.github.com>
2019-12-17 21:14:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,9 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class Mee {
   public static void main(String[] args) {
-    System.out.println("Hello, World")
+    System.out.println("Hello, World");
     System.out.println("Hello, pooja");
   }
 }
